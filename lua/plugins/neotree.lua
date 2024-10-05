e58bdc288f717d759703070446707d7041844c1e
Shahan <shahan.khaan@gmail.com>
2024-10-05 20:22:01 +0500

    fix: always allow dot files in neotree, and update lock file

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,12 +1,25 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  branch = "v3.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-    "MunifTanjim/nui.nvim",
-  },
-  config = function()
-    vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal right<CR>', {})
-  end
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		require("neo-tree").setup({
+			filesystem = {
+				filtered_items = {
+					visible = true,
+					show_hidden_count = true,
+					hide_dotfiles = true,
+					hide_gitignored = false,
+					never_show = {
+						".git",
+					},
+				},
+			},
+		})
+		vim.keymap.set("n", "<leader>n", ":Neotree filesystem reveal right<CR>", {})
+	end,
 }
